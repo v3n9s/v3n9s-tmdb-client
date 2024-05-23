ed333ea85916c482b51ffb95cb264e6b9735135b
Venyamin Shvets <venyashvets@gmail.com>
2024-05-23 20:03:57 +0300

    implement not found page

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -1,7 +1,40 @@
+import {
+  Button,
+  Center,
+  Image,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import type { FC } from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage: FC = () => {
-  return "not found page";
+  const theme = useMantineTheme();
+
+  return (
+    <Center h="900px">
+      <Stack align="center" gap="0">
+        <Image src="/404.svg" />
+        <Text fz="20" fw="700" mt="35">
+          We can&apos;t find the page you are looking for
+        </Text>
+        <Button
+          component={Link}
+          to="/movies"
+          bg={theme.other.colors.purple500}
+          c={theme.other.colors.white}
+          w="103"
+          h="40"
+          mt="13"
+          radius="8"
+          fw="400"
+        >
+          Go Home
+        </Button>
+      </Stack>
+    </Center>
+  );
 };
 
 export default NotFoundPage;
